a065248277ead9a226ad6a831c4d186d8b6c2cac
littlejeem <jlivin25@live.co.uk>
2020-05-07 15:04:32 +0100

    removed extra source and all working, version 1

--- handbrake_folder.sh
+++ handbrake_folder.sh
@@ -2,13 +2,12 @@
 
 # this script is to automatically convert a folder of video files
 # You need to change SRC -- Source folder and DEST -- Destination folder
-VERSION="0.8"
+VERSION="1.0"
 SRC="/media/Data_1/Videos/TV_Shows/Chernobyl/Season 1"
 DEST="/home/jlivin25/Videos"
 DEST_EXT="mp4"
 HANDBRAKE_CLI=HandBrakeCLI
 PRESET="Very Fast 1080p30"
-#HandBrakeCLI -i /media/Data_1/Videos/TV_Shows/Chernobyl/Season\ 1/S01E01\ -\ 1-23-45.mkv -o /home/jlivin25/Videos/S01E01\ -\ 1-23-45.mkv --preset "Very Fast 1080p30"
 #
 #
 for FILE in "$SRC"/*
