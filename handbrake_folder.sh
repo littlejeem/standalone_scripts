41586611efb24a2c2652583e4c491e7ea6b4b61d
littlejeem <jlivin25@live.co.uk>
2020-05-12 23:15:52 +0100

    corrected error with preset not enacting

--- handbrake_folder.sh
+++ handbrake_folder.sh
@@ -2,7 +2,7 @@
 
 # this script is to automatically convert a folder of video files
 # You need to change SRC -- Source folder and DEST -- Destination folder
-VERSION="1.0"
+VERSION="1.1"
 SRC="" #<--- include spaces but no trailing slash
 DEST="" #<--- include spaces but no trailing slash
 DEST_EXT="mp4" #<--- .mp4 extension by default
@@ -15,5 +15,5 @@ do
  filename=$(basename "$FILE")
  extension=${filename##*.}
  filename=${filename%.*}
-$HANDBRAKE_CLI -i "$FILE" -o "$DEST/$filename.$DEST_EXT" "$PRESET"
+$HANDBRAKE_CLI -i "$FILE" -o "$DEST/$filename.$DEST_EXT" -Z "$PRESET"
 done
