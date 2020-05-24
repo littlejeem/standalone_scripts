a1c1dc6092a3f5fb22d039d732fde7877f43bc59
James Livingstone <jlivin25@live.co.uk>
2020-05-24 16:35:57 +0100

    a small change to correct script function

--- makemkv-key-update.sh
+++ makemkv-key-update.sh
@@ -7,7 +7,6 @@
 FILE_LOC=/home/jlivin25/.MakeMKV/settings.conf
 #URL='https://www.makemkv.com/forum/viewtopic.php?t=1053'
 URLCONTENT=$(wget https://www.makemkv.com/forum/viewtopic.php?t=1053 -q -O -)
-#KEY="`lynx -source $URL | grep 'codecontent">' | cut -d \> -f11 | sed 's/<\/div$//'`"
-KEY=echo "$URLCONTENT" | grep -o -P '(?<=<code>).*(?=</code>)'
-echo $KEY
+KEY=$(echo "$URLCONTENT" | grep -o -P '(?<=<code>).*(?=</code>)')
+echo "$KEY"
 sed -i "s|^app_Key.*|${KEY}|" /home/jlivin25/.MakeMKV/settings.conf
