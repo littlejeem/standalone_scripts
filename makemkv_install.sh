9b08c35db8f3e87eeb4fd7cb82199cd3b6d486aa
James Livingstone <jlivin25@live.co.uk>
2020-05-24 23:31:43 +0100

    additional functionality to grab version, accounted running as root

--- makemkv_install.sh
+++ makemkv_install.sh
@@ -3,6 +3,7 @@
 #
 #
 topdir="/home/jlivin25"
+assigned_user="jlivin25"
 #+-----------------------------------------------+
 #+----check running as root before continuing----+
 #+-----------------------------------------------+
@@ -13,14 +14,22 @@ if [[ $UID != 0 ]]; then
 fi
 #
 #
+#+--------------------------------+
+#+----FInd the current version----+
+#+--------------------------------+
+urlcontent=$(wget https://www.makemkv.com/forum/viewtopic.php?f=3&t=224 -q -O -)
+version_grab=$(echo "$urlcontent" | grep -o -P '(?<=<title>).*(?=</title>)')
+version=${version_grab:8:6}
+#
+#
 #+-------------------------+
 #+----Start Main Script----+
 #+-------------------------+
 cd ~/
-mkdir -p "$topdir"/Downloads
+sudo -u $assigned_user mkdir -p "$topdir"/Downloads
 cd "$topdir"/Downloads
-wget https://www.makemkv.com/download/makemkv-bin-1.15.1.tar.gz
-wget https://www.makemkv.com/download/makemkv-oss-1.15.1.tar.gz
+wget https://www.makemkv.com/download/makemkv-bin-"$version".tar.gz
+wget https://www.makemkv.com/download/makemkv-oss-"$version".tar.gz
 mkdir -p makemkv-bin
 mkdir -p makemkv-oss
 tar xf makemkv-bin-*.tar.gz -C makemkv-bin --strip-components 1
@@ -28,12 +37,19 @@ tar xf makemkv-oss-*.tar.gz -C makemkv-oss --strip-components 1
 rm makemkv-*-*.tar.gz
 #
 #
+#+-------------------------+
+#+---Sort out ownerships---+
+#+-------------------------+
+chown -R "$assigned_user":"$assigned_user" "$topdir"/Downloads
+chmod -R 766 "$topdir"/Downloads
+#
+#
 #+---------------+
 #+---build OSS---+
 #+---------------+
 cd "$topdir"/Downloads/makemkv-oss
-./configure
-make
+sudo -u $assigned_user ./configure
+sudo -u $assigned_user make
 sudo make install
 #
 #
@@ -41,5 +57,5 @@ sudo make install
 #+---build BIN---+
 #+---------------+
 cd "$topdir"/Downloads/makemkv-bin
-make
+sudo -u $assigned_user make
 sudo make install
