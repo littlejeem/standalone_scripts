e2b553b1ca689c3a524d5b06b0904d8141022422
James Livingstone <jlivin25@live.co.uk>
2020-05-29 09:51:38 +0100

    Amended layout, altered code line to correct non working

--- batch-image-resize.sh
+++ batch-image-resize.sh
@@ -2,24 +2,35 @@
 # Purpose: batch image resizer
 # Source: https://guides.wp-bullet.com
 # Author: Mike
-
+#
 # absolute path to image folder
 FOLDER="/media/Data_1/KODI_Slideshow"
-
+#
 # max width
 WIDTH=540
-
+#
 # max height
 HEIGHT=300
-
-#resize png or jpg to either height or width, keeps proportions using imagemagick
-find ${FOLDER} -iname '*.jpg' -o -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
-
+#
+#
+#+----------------------+
+#+---Code Explanation---+
+#+----------------------+
+#Find Source           FileSearch Escape   nonCASEsens filetype1 OR  nonCASEsens filetype2 Escape Executecommand convert filename Detail   ResizePic  Resizetowhat(% or PIXxPIX) filename end-exec
+#find ${source_folder} -type f    \(       -iname      \*.jpg    -o  -iname      \*.png    \)     -exec          convert \{}      -verbose -resize    25%\>                      \{}      \;
+#
+#
+#+----------------------+
+#+---Two Type Convert---+
+#+----------------------+
+#resize png or jpg to % of original using imagemagick
+find ./ -type f \( -iname \*.jpg -o -iname \*.png \) -exec convert \{} -verbose -resize 25%\> \{} \;
+#
+#
+#+----------------------+
+#+---One Type Convert---+
+#+----------------------+
 #resize png to either height or width, keeps proportions using imagemagick
-#find ${FOLDER} -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
-
+#find ./ -type f \( -iname \*.png \) -exec convert \{} -verbose -resize 25%\> \{} \;
 #resize jpg only to either height or width, keeps proportions using imagemagick
-#find ${FOLDER} -iname '*.jpg' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
-
-# alternative
-#mogrify -path ${FOLDER} -resize ${WIDTH}x${HEIGHT}% *.png -verbose
+#find ./ -type f \( -iname \*.jpg \) -exec convert \{} -verbose -resize 25%\> \{} \;
