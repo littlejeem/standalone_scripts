2c8d5618a4a9f4690849963ec0f0eb78df27aa50
James Livingstone <jlivin25@live.co.uk>
2020-06-02 12:22:25 +0100

    should fix #9

--- makemkv-key-update.sh
+++ makemkv-key-update.sh
@@ -5,9 +5,9 @@
 ### A script to automatically update the key MakeMKV beta ###
 #############################################################
 file_loc=/home/jlivin25/.MakeMKV/settings.conf
-#URL='https://www.makemkv.com/forum/viewtopic.php?t=1053'
 url_content=$(wget https://www.makemkv.com/forum/viewtopic.php?t=1053 -q -O -)
 key=$(echo "$url_content" | grep -o -P '(?<=<code>).*(?=</code>)')
 echo "$key"
 keyinsert=('"'$key'"')
-sed -i 's/".*"/\'$keyinsert'/' settings.conf
+touch $file_loc
+sed -i 's/".*"/\'$keyinsert'/' $file_loc
