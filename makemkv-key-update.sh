bf6f8f8ac3415a2f70918552ea5f15c8e351b55b
James Livingstone <jlivin25@live.co.uk>
2020-06-02 14:24:58 +0100

    annotated comments

--- makemkv-key-update.sh
+++ makemkv-key-update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 #
-#############################################################
-### A script to automatically update the key MakeMKV beta ###
-#############################################################
+###################################################################
+### "A script to automatically update the key for MakeMKV beta" ###
+###################################################################
 file_loc=/home/jlivin25/.MakeMKV/settings.conf
 url_content=$(wget https://www.makemkv.com/forum/viewtopic.php?t=1053 -q -O -)
 key=$(echo "$url_content" | grep -o -P '(?<=<code>).*(?=</code>)')
