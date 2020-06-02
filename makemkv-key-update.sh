8c6d6d789b20adf8601054fec3dfc9bd9550c84c
James Livingstone <jlivin25@live.co.uk>
2020-06-02 14:14:20 +0100

    fix #10 - test for non existant settings file and if existing but empty

--- makemkv-key-update.sh
+++ makemkv-key-update.sh
@@ -9,5 +9,23 @@ url_content=$(wget https://www.makemkv.com/forum/viewtopic.php?t=1053 -q -O -)
 key=$(echo "$url_content" | grep -o -P '(?<=<code>).*(?=</code>)')
 echo "$key"
 keyinsert=('"'$key'"')
-touch $file_loc
-sed -i 's/".*"/\'$keyinsert'/' $file_loc
+if test -f "$file_loc"
+then
+  echo "$file_loc exists"
+  if [ -s "$_file" ]
+  then
+  	echo "$file_loc not empty so inserting key"
+    sed -i 's/".*"/\'$keyinsert'/' $file_loc
+  else
+  	echo "$_file is empty, inserting text"
+    echo 'appKey:""' > $file_loc
+    sed -i 's/".*"/\'$keyinsert'/' $file_loc
+  fi
+else
+  echo "$file_loc doesn't exist, creating it"
+  touch $file_loc
+  echo "inserting appKey variable"
+  echo 'appKey:""' > $file_loc
+  echo "inserting key"
+  sed -i 's/".*"/\'$keyinsert'/' $file_loc
+fi
