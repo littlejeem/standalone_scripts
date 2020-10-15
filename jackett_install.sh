669276aa635b73071cbd7bd49d94a98ddb490b96
James Livingstone <jlivin25@live.co.uk>
2020-10-15 22:59:13 +0100

    added functionality for testing

--- jackett_install.sh
+++ jackett_install.sh
@@ -5,21 +5,37 @@
 #+-------------------+
 #+---Source helper---+
 #+-------------------+
-source ./help_script.sh
+source ./helper_script.sh
 #
 #
 #+---------------------+
 #+---"Set Variables"---+
 #+---------------------+
+SCRIPTENTRY
+sctipt_log="/home/pi/bin/scriptlogs/jackett_install.log"
 stamp=$(Timestamp)
+#
+#
 cd /opt
 target https://github.com/Jackett/Jackett/releases/download/v0.16.1724/Jackett.Binaries.LinuxAMDx64.tar.gz
 jackettver=$(wget -q https://github.com/Jackett/Jackett/releases/latest -O - | grep -E \/tag\/ | awk -F "[><]" '{print $3}')
 wget -q https://github.com/Jackett/Jackett/releases/download/$jackettver/Jackett.Binaries.LinuxAMDx64.tar.gz
 cp ~/.config/Jackett/ServerConfig.json ~/ServerConfig.json
 systemctl stop jackett.service
-mv Jackett Jackett_$stamp
+#
+if [ -d "Jackett" ]; then
+  ENTRY
+  mv Jackett Jackett_$stamp
+  EXIT
+fi
 tar -xvf Jackett.tar
+INFO
 systemctl start jackett.service
+INFO
 rm ~/ServerConfig.json
+INFO
 rm Jackett.tar
+INFO
+#
+#
+SCRIPTEXIT
