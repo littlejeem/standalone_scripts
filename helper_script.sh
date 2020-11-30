ed69a5782f52c4515fd248e582eb2cdece6081ab
James Livingstone <jlivin25@live.co.uk>
2020-11-30 22:01:42 +0000

    removed redundant functions, moved to logger/terminal based capture

--- helper_script.sh
+++ helper_script.sh
@@ -31,60 +31,10 @@ nc='\033[0m'
 #+-----------------------+
 #+---Logging Functions---+
 #+-----------------------+
-SCRIPTENTRY ()
-{
- timeAndDate=$(date)
- script_name="demo2.sh"
- script_name="${script_name%.*}"
- echo "[$timeAndDate] [DEBUG] > $script_name $FUNCNAME" >> $SCRIPT_LOG
-}
-#
-SCRIPTEXIT ()
-{
- script_name="demo2.sh"
- script_name="${script_name%.*}"
- echo "[$timeAndDate] [DEBUG] < $script_name $FUNCNAME" >> $SCRIPT_LOG
-}
-#
-ENTRY ()
-{
-local cfn="${FUNCNAME[1]}"
-#cfn="${FUNCNAME[1]}"
- timeAndDate=$(date)
- echo "[$timeAndDate] [DEBUG] > $cfn $FUNCNAME" >> $SCRIPT_LOG
-}
-#
-EXIT ()
-{
-local cfn="${FUNCNAME[1]}"
-# cfn="${FUNCNAME[1]}"
- timeAndDate=$(date)
- echo "[$timeAndDate] [DEBUG] < $cfn $FUNCNAME" >> $SCRIPT_LOG
-}
-#
-INFO ()
-{
- local function_name="${FUNCNAME[1]}"
-   local msg="$1"
-    timeAndDate=$(date)
-    echo "[$timeAndDate] [INFO]  $msg" >> $SCRIPT_LOG
-}
-#
-DEBUG ()
-{
- local function_name="${FUNCNAME[1]}"
-   local msg="$1"
-    timeAndDate=$(date)
- echo "[$timeAndDate] [DEBUG]  $msg" >> $SCRIPT_LOG
-}
-#
-ERROR ()
-{
- local function_name="${FUNCNAME[1]}"
-   local msg="$1"
-    timeAndDate=$(date)
-    echo "[$timeAndDate] [ERROR]  $msg" >> $SCRIPT_LOG
-}
+tty -s && function log()     {     echo "$@"; }
+tty -s && function log_err() { >&2 echo "$@"; }
+tty -s || function log()     { logger -t INFO $(basename $0) "$@"; }
+tty -s || function log_err() { logger -t DEBUG $(basename $0) -p user.err "$@"; }
 #
 #
 #+------------------------+
