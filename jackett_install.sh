66299ed6a05576da4c7e7d2b532d9f583b726a53
James Livingstone <jlivin25@live.co.uk>
2020-12-14 22:35:34 +0000

    version for testing

--- jackett_install.sh
+++ jackett_install.sh
@@ -2,6 +2,16 @@
 #
 # taken from here https://www.htpcguides.com/install-jackett-ubuntu-15-x-for-custom-torrents-in-sonarr/
 #
+#+---------------------------------------------+
+#+---check running as root before continuing---+
+#+---------------------------------------------+
+if [[ $EUID -ne 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+#
+#
 #+-------------------+
 #+---Source helper---+
 #+-------------------+
@@ -13,6 +23,7 @@ source ./helper_script.sh
 #+---------------------+
 SCRIPT_LOG="/home/pi/bin/logs/jackett_install.log"
 stamp=$(Timestamp)
+PATH=/sbin:/bin:/usr/bin:/home/jlivin25:/home/jlivin25/.local/bin:/home/jlivin25/bin
 #
 #
 jackett_ver=$(wget -q https://github.com/Jackett/Jackett/releases/latest -O - | grep -E \/tag\/ | awk -F "[><]" '{print $3}')
@@ -33,9 +44,55 @@ if [ -d "/opt/Jackett" ]; then
   rm Jackett.tar
 else
   log "No Jackett install detected, attempting install"
-  cd /home/jlivin25/Downloads
+  cd /opt
+  log "downloading version $jackett_target"
   wget -q https://github.com/Jackett/Jackett/releases/download/$jackett_target/Jackett.Binaries.LinuxAMDx64.tar.gz
-  tar -xvf Jackett*.tar
-  mv Jackett /opt/
-  chmod USER:USER /opt/Jackett
+  tar -xvf Jackett*
+#  mv Jackett /opt/
+#  if [[ $? -eq 1 ]]; then
+#    log_err "moving to /opt/ failed, exiting..."
+#    exit 1
+#  fi
+  chmod $USER:$USER /opt/Jackett
+  if [[ $? -eq 1 ]]; then
+    log_err "chowning /opt/Radarr, exiting..."
+    exit 1
+  fi
+  log "creating .service file"
+  cat > /etc/systemd/system/jackett.service <<-EOF
+  [Unit]
+  Description=Jackett Daemon
+  After=network.target
+
+  [Service]
+  SyslogIdentifier=jackett
+  Restart=always
+  RestartSec=5
+  Type=simple
+  User=$USER
+  Group=$USER
+  WorkingDirectory=/opt/Jackett
+  ExecStart=/opt/Jackett/jackett --NoRestart
+  TimeoutStopSec=20
+
+  [Install]
+  WantedBy=multi-user.target
+  EOF
+  if [[ $? -eq 1 ]]; then
+    log_err "creating .service file failed, exiting..."
+    exit 1
+  else
+    log "creating .service file"
+  fi
+  log "starting service"
+  systemctl daemon-reload
+  systemctl start jacket.service
+  if [[ $? -ne 0 ]]; then
+    log_err "starting service failed, exiting..."
+    exit 1
+  else
+    log "Started service file successfully"
+  fi
+
+
 fi
