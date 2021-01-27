9834f88ca2dc2e50fd16569882793a49b0f8223c
James Livingstone <littlejeem@protonmail.com>
2021-01-27 19:21:49 +0000

    renamed to modern function names, added exit code evaluation

--- helper_script.sh
+++ helper_script.sh
@@ -33,7 +33,7 @@ tty -s || function log_err() { logger -t ERROR $(basename $0) -p user.err "$@"; 
 #+------------------------+
 #+---Pushover Functions---+
 #+------------------------+
-function Pushover ()
+pushover ()
 {
   curl -s --form-string token="$backup_app_token" --form-string user="$user_token" --form-string message="$message_form" https://api.pushover.net/1/messages.json
 }
@@ -45,9 +45,43 @@ function Pushover ()
 #
 #As this is used to replicate syslog in Logging Functiosn should it not be the same format? <-- Dec  7 20:48:52 testbed-1804
 #
-function Timestamp ()
+timestamp ()
 {
   echo "$(date +%b"  "%-d" "%T)" " "$1
 }
 #
 #
+#+------------------+
+#+---"Exit Codes"---+
+#+------------------+
+#
+# pick from 64 - 113
+# exit 0 = Success
+# exit 64 = Variable Error
+# exit 65 = Sourcing file error
+# exit 66 = Processing Error
+# exit 67 = Required Program Missing
+#
+#place this in parent script immediately after the child script exits
+script_exit ()
+{
+  reply=$?
+  if [[ "$reply" = 0 ]]; then
+    log "$scriptlong exited gracefully"
+  elif [ "$reply" = 64 ]]; then
+    log_err "Exit code: $reply received"
+    log_deb "Script $scriptlong exited with 'Variable' error"
+  elif [ "$reply" = 65 ]]; then
+    log_err "Exit code: $reply received"
+    log_deb "Script $scriptlong exited with 'sourcing script or config' error"
+  elif [ "$reply" = 65 ]]; then
+    log_err "Exit code: $reply received"
+    log_deb "Script $scriptlong exited with 'Processing' error"
+  elif [ "$reply" = 66 ]]; then
+    log_err "Exit code: $reply received"
+    log_deb "Script $scriptlong exited with 'Missing Program' error"
+  elif [ "$reply" = 1 ]]; then
+    log_err "Exit code: $reply received"
+    log_deb "Script $scriptlong exited with generic bash error"
+  fi
+}
