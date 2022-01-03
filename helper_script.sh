d35705a9713123a85556aa80c2857e46d8ca0b0f
James Livingstone <littlejeem@protonmail.com>
2022-01-03 14:41:49 +0000

    new progress bar, tidied up comments

--- helper_script.sh
+++ helper_script.sh
@@ -9,27 +9,43 @@
 ### e.g: sudo ln -s /home/$USER/bin/standalone_scripts/helper_script.sh /usr/local/bin/helper_script.sh ###
 ###########################################################################################################
 #
-#+-------------+
-#+---Version---+
-#+-------------+
-version=0.2
+#+---------------+
+#+---"VERSION"---+
+#+---------------+
+version=0.3
 #
 #
-#+---------------------+
-#+---Logging Colours---+
-#+---------------------+
+#+-----------------------+
+#+---"LOGGING COLOURS"---+
+#+-----------------------+
+#KEY
+#Black        0;30     Dark Gray     1;30
+#Red          0;31     Light Red     1;31
+#Green        0;32     Light Green   1;32
+#Brown/Orange 0;33     Yellow        1;33
+#Blue         0;34     Light Blue    1;34
+#Purple       0;35     Light Purple  1;35
+#Cyan         0;36     Light Cyan    1;36
+#Light Gray   0;37     White         1;37
 colwht='\033[1;37m' # White - Regular
 colblk='\033[0;30m' # Black - Regular
 colred='\033[0;31m' # Red
+#red='\033[0;31m'
 colgrn='\033[0;32m' # Green
+#green='\033[0;32m'
 colylw='\033[0;33m' # Yellow
+#yellow='\033[1;33m'
+#brown_orange='\033[0;33m'
+#light_blue='\033[1;34m'
 colpur='\033[0;35m' # Purple
+#purple='\033[0;35m'
 colrst='\033[0m'    # Text Reset
+#nc='\033[0m' # No Color
 #
 #
-#+-----------------------+
-#+---Logging Functions---+
-#+-----------------------+
+#+-------------------------+
+#+---"LOGGING FUNCTIONS"---+
+#+-------------------------+
 # all credit here: http://www.ludovicocaldara.net/dba/bash-tips-4-use-logging-levels/
 #
 ### verbosity levels
@@ -78,32 +94,10 @@ slog() {
 #
 #
 #
-#+-------------------------------+
-#+---"Check if already runnng"---+
-#+-------------------------------+
-# Function for insertion at beginning of scripts to check for existing tmp directory named using 'lockname' at start of script
-# If detects folder it will put the usings script into a holding 'while' loop until already running script exits
-# Using a folder as this is create using atomic timing and helps prevent race conditions
-check_running () {
-  if [[ -d /tmp/"$lockname" ]]; then
-    while [[ -d /tmp/"$lockname" ]]; do
-      ewarn "previous script still running"
-      sleep 2m; done
-      #  else
-      edebug "no previously running script detected"
-  fi
-  edebug "Attempting to lock script"
-  mkdir /tmp/"$lockname"
-  if [[ $? = 0 ]]; then
-    edebug "temp dir is set as successfully as: /tmp/$lockname"
-  else
-    eerror "setting temp directory unsuccessfull, exiting"
-    exit 65
-  fi
-}
-#+------------------------+
-#+---Pushover Functions---+
-#+------------------------+
+
+#+--------------------------+
+#+---"PUSHOVER FUNCTIONS"---+
+#+--------------------------+
 pushover ()
 {
   curl -sS --form-string token="$application_token" --form-string user="$user_token" --form-string title="$pushover_title" --form-string message="$message_form" https://api.pushover.net/1/messages.json > /dev/null
@@ -111,7 +105,7 @@ pushover ()
 #
 #
 #+----------------------+
-#+---"Kodi Functions"---+
+#+---"KODI FUNCTIONS"---+
 #+----------------------+
 update_videolibrary () {
   curl -sS --data-binary '{ "jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "mybash"}' -H 'content-type: application/json;' $kodi_VIDEO_assembly > /dev/null
@@ -143,7 +137,7 @@ timestamp ()
 #
 #
 #+------------------+
-#+---"Exit Codes"---+
+#+---"EXIT CODES"---+
 #+------------------+
 #
 # pick from 64 - 113 (https://tldp.org/LDP/abs/html/exitcodes.html#FTN.AEN23647)
@@ -180,8 +174,30 @@ script_exit ()
 #
 #
 #+--------------------------------+
-#+---"Variable & Program Tests"---+
+#+---"VARIABLE & PROGRAM TESTS"---+
 #+--------------------------------+
+#"Check if already runnng"
+# Function for insertion at beginning of scripts to check for existing tmp directory named using 'lockname' at start of script
+# If detects folder it will put the usings script into a holding 'while' loop until already running script exits
+# Using a folder as this is create using atomic timing and helps prevent race conditions
+check_running () {
+  if [[ -d /tmp/"$lockname" ]]; then
+    while [[ -d /tmp/"$lockname" ]]; do
+      ewarn "previous script still running"
+      sleep 2m; done
+      #  else
+      edebug "no previously running script detected"
+  fi
+  edebug "Attempting to lock script"
+  mkdir /tmp/"$lockname"
+  if [[ $? = 0 ]]; then
+    edebug "temp dir is set as successfully as: /tmp/$lockname"
+  else
+    eerror "setting temp directory unsuccessfull, exiting"
+    exit 65
+  fi
+}
+#
 fatal_missing_var () {
  if [[ -z "${JAIL_FATAL}" ]]; then
   eerror "Failed to find: $JAIL_FATAL, JAIL_FATAL is unset or set to the empty string, script cannot continue. Exiting!"
@@ -233,8 +249,10 @@ Check_Service_SubState () {
 #
 #
 #+----------------------+
-#+---"Progress bar"-----+
+#+---"PROGRESS BAR"-----+
 #+----------------------+
+#
+# "PROGRESS BAR 1"
 #thanks to here https://stackoverflow.com/questions/12498304/using-bash-to-display-a-progress-indicator
 #913476814 = 872mb on bytes
 progress_bar () {
@@ -251,8 +269,93 @@ progress_bar () {
   echo ""
 }
 #
+# "PROGRESS BAR 2"
+# for this all credit see answer from 'Vagiz Duseev' at https://stackoverflow.com/questions/238073/how-to-add-a-progress-bar-to-a-shell-script
+# Need to pass in two functions (as cant use a variable as a command in bash), get_max_progress & get_total_progress then call progress_bar2_init
+# immediately after pid'ding your command you want progress from, structure goes:
+# 1: put 2x functions at top of script, examples below
+# 2: unit_of_measure=""
+# 3: yourcommandhere > /dev/null 2>&1 &
+# 4: yourcommandhere_pid=$!
+# 5: pid_name=$yourcommandhere
+# 6: progress_bar2_init
+#
+#get_max_progress () {
+#  tail -n 1 "$working_dir/temp/$bluray_name/$bluray_name.log" | cut -d ',' -f 3
+#}
+#
+#get_total_progress () {
+#  tail -n 1 "$working_dir/temp/$bluray_name/$bluray_name.log" | cut -d ',' -f 2
+#}
+#
+progress_bar2_init () {
+  while kill -0 $pid_name 2> /dev/null; do
+    progress_max=$(get_max_progress)
+    progress_total=$(get_total_progress)
+
+    #DEBUG STUFF
+    #remove characters / collums for: "[", " ] " "100% " "("$progress_total " / " $progress_max " "$unit_of_measure")"
+    count_standard_deduction=15 #1x"(", 1x"[", 1x")", 1x"]", 1x"100%", 1x"/", 6x " "
+    edebug "deduct standard: $count_standard_deduction, should read 9"
+    count_progress_max=$(echo ${#progress_max})
+    edebug "count_progress_max is: $count_progress_max , should read 5"
+    unit_of_measure=$(echo ${#unit_of_measure})
+    edebug "unit of measure is: $unit_of_measure, should read cycles"
+    count_unit_of_measure=$(echo ${#unit_of_measure})
+    edebug "count_unit_of_measure is: $count_unit_of_measure , should read 6"
+
+    #SCREEN SIZING
+    screen_width=$(stty size | cut -d" " -f2)
+    edebug "original screen width is: $screen_width"
+    screen_width_deduction=$(($count_standard_deduction + $count_unit_of_measure + $count_progress_max + $count_progress_max))
+    edebug "knocking off: $screen_width_deduction , should equal $count_standard_deduction + $count_unit_of_measure + $count_progress_max + $count_progress_max"
+    screen_width=$(($screen_width - $screen_width_deduction))
+    edebug "final screen progress bar will be: $screen_width"
+
+    #for use cases such as makemkv where the progress file is 'dirty' and could be text, do a test
+    #if either progress_total or progress_max are NOT equal to a number enter the loop
+    num_check='^[0-9]+$'
+    while ! [[ $progress_total =~ $num_check ]] || ! [[ $progress_max =~ $num_check ]] ; do
+      sleep .1
+      progress_max=$(get_max_progress)
+      progress_total=$(get_total_progress)
+    done
+
+    # Draw a progress bar
+    progress_bar2 $progress_total $progress_max $unit_of_measure
+
+    # Check if we reached 100%
+    if [ $progress_total == $progress_max ]; then break; fi
+    sleep 1  # Wait before redrawing
+  done
+# Go to the newline at the end of task
+printf "\n"
+}
+#
+progress_bar2 () {
+  # Arguments: current value, max value, unit of measurement (optional)
+  local __value=$1
+  local __max=$2
+  local __unit=${3:-""}  # if unit is not supplied, do not display it
+
+  # Calculate percentage
+  if (( $__max < 1 )); then __max=1; fi  # anti zero division protection
+  local __percentage=$(( 100 - ($__max*100 - $__value*100) / $__max ))
+
+  # Rescale the bar according to the progress bar width
+  local __num_bar=$(( $__percentage * $screen_width / 100 ))
+
+  # Draw progress bar
+  printf "["
+  for b in $(seq 1 $__num_bar); do printf "▓"; done
+  for s in $(seq 1 $(( $screen_width - $__num_bar ))); do printf " "; done
+  printf "] $__percentage%% ($__value / $__max $__unit)\r"
+}
 #
 #
+#+------------------+
+#+---"IP STUFF"-----+
+#+------------------+
 # perhaps add this to alias just like SE answer here?
 #https://unix.stackexchange.com/questions/22615/how-can-i-get-my-external-ip-address-in-a-shell-script
 int_ip () {
@@ -272,7 +375,7 @@ wan_ip6 () {
 }
 #
 #+------------------------+
-#+---"Useful Variables"---+
+#+---"USEFUL VARIABLES"---+
 #+------------------------+
 #logname=$lockname.log # Uses the script name to create the log
 #stamp=$(echo "SYNC-`date +%d_%m_%Y`-`date +%H.%M.%S`")
